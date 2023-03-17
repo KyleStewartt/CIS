d5db87905e54809391e98e48fab1590da9b47f55
Kyle Stewart <sk55690@mail.harpercollege.edu>
2023-03-16 21:43:07 -0600

    5 done

--- PS9/PS9P5.py
+++ PS9/PS9P5.py
@@ -1,1 +1,23 @@
+def compute_tuition_owed(credit_hours, district_code):
+    if district_code == 'I':
+        return credit_hours * 250
+    elif district_code == 'O':
+        return credit_hours * 550
+    else:
+        return None
+
+def main():
+    last_name = input("Enter student's last name: ")
+    credit_hours = int(input("Enter credit hours: "))
+    district_code = input("Enter district code (I for In district, O for Out of district): ").upper()
+
+    tuition_owed = compute_tuition_owed(credit_hours, district_code)
+    
+    if tuition_owed is None:
+        print("Invalid district code. Please try again.")
+    else:
+        print(f"Student {last_name}'s tuition owed: ${tuition_owed:.2f}")
+
+if __name__ == "__main__":
+    main()
 
